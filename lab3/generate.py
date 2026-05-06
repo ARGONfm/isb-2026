41d777cd0aed84f09667be97f30f85171c3f712a
ARGON <165267470+ARGONfm@users.noreply.github.com>
2026-05-06 14:07:56 +0400

    Update generate.py

--- lab3/generate.py
+++ lab3/generate.py
@@ -6,6 +6,7 @@ from file_util import ensure_directory
 from asymmetric import encrypt_symmetric_key
 
 def generate_symmetric_key(key_bits):
+    "Генерация симметричного ключа"
     if key_bits < 32 or key_bits > 448:
         raise ValueError("Длина ключа должна быть в пределах допустимого (32 - 448 бита)\n")
     
@@ -19,6 +20,7 @@ def generate_symmetric_key(key_bits):
     return key
 
 def generate_rsa_keys():
+    "Генерация пары ассиметричных RSA ключей"
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
@@ -29,6 +31,7 @@ def generate_rsa_keys():
     return private_key, public_key
 
 def save_private_key(private_key, filepath):
+    "Сереализация закрытого ключа"
     try:
         with open(filepath, 'wb') as private_out:
             private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
@@ -41,6 +44,7 @@ def save_private_key(private_key, filepath):
         return False
 
 def save_public_key(public_key, filepath):
+    "Сереализация открытого ключа"
     try:
         with open(filepath, 'wb') as public_out:
             public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
