f0e6fd7342fa70f617b0e41e879fd7bd9a972c3a
ARGON <165267470+ARGONfm@users.noreply.github.com>
2026-05-06 13:11:26 +0400

    Update and rename encrypt.py to asymmetric.py

---
+++ lab3/asymmetric.py
@@ -0,0 +1,43 @@
+import os 
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding as asymmetric_padding
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from file_util import ensure_directory
+
+
+
+def load_private_key(filepath):
+    try:
+        with open (filepath, 'rb') as pem_in:
+            private_bytes = pem_in.read()
+            private_key = load_pem_private_key(private_bytes,password=None,)
+            print (f"Десериализован закрытый ключ из {filepath} \n")
+            return private_key
+
+    except Exception as error:
+        print(f"Не удалось десериализовать закрытый ключ {filepath}: {error}\n")
+        return None
+
+def encrypt_symmetric_key(symmetric_key, public_key, filepath):
+    try: 
+        with open(filepath, 'wb') as key_file:
+            encrypt_key = public_key.encrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+            key_file.write(encrypt_key)
+        return True
+
+    except Exception as error:
+        print(f"Не удалось сериализовать симмтричный ключ в {filepath}: {error}\n")
+        return False
+
+def decrypt_symmetric_key (filepath, private_key):
+    try:
+        with open (filepath, mode='rb') as key_file:
+            symmetric_bytes = key_file.read()
+            symmetric_key = private_key.decrypt(symmetric_bytes,asymmetric_padding.OAEP(mgf=asymmetric_padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+            print (f"Десериализован симметричный ключ из {filepath} \n")
+            return symmetric_key
+
+    except Exception as error:
+        print(f"Не удалось десериализовать симметричный ключ {filepath}: {error}\n")
+        return None
+
